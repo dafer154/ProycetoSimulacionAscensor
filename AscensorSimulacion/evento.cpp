4390915bcb7cd881368220bf7a9fc70db2880ed9
dafer154 <david_fza@hotmail.com>
2014-11-30 00:13:41 -0500

    Eventos
    Pruebas de Eventos

--- AscensorSimulacion/evento.cpp
+++ AscensorSimulacion/evento.cpp
@@ -3,3 +3,72 @@
 Evento::Evento()
 {
 }
+Evento::~Evento()
+{
+}
+
+
+void Evento::llegadaPersonaPiso()
+{
+
+    generarllegadaPersonaPiso; // se genera en el LEF
+
+    int calcularPisoActualPersona(); //metodo que calcula pisoActualPersona
+
+    int ColaAfuera[PisoActualPersona]; //metodo que calcula colaAfuera
+
+    entradaPersonaAscensor(); //
+
+}
+
+void Evento::entradaPersonaAscensor()
+{
+
+    int capacidadOcupada=0;
+    int capacidadOcupadaMaxima=0;
+    int maxima=0;
+    int pisoDestinoAscensor=0;
+    int pisoDestinoPersona=0;
+
+  if((capacidadOcupada==capacidadOcupadaMaxima)||(colaAfuera[pisoActual]== 0))
+
+      pisoDestinoAscensor;
+      generarCambioPisoAscensor();
+
+      else
+
+      colaAfuera[pisoActual];
+
+      pisoDestinoPersona;//aleatorio
+
+      colaAscensor[pisoDestinoPersona];//aumente
+
+      capacidadOcupada ++;
+
+      generarEntradaPersonaAscensor;
+
+}
+
+void Evento::cambioPisoAscensor()
+{
+
+    int pisoActualAscensor=0;
+    int pisoDestinoAscensor=0;
+    int colaAscensor=0;
+    int atendidos=0;
+    int capacidadOcupada=0;
+
+    pisoActualAscensor=pisoDestinoAscensor;
+
+    if(colaAscensor[pisoActual]==0)
+
+        entradaPersonaAscensor();
+
+    else
+
+        atendidos ++;
+
+        colaAscensor[pisoActual]--;
+        capacidadOcupada --;
+}
+
--- AscensorSimulacion/evento.h
+++ AscensorSimulacion/evento.h
@@ -4,7 +4,14 @@
 class Evento
 {
 public:
+
     Evento();
-};
+
+   char tipoEvento;//
+   int tiempo;//
+
+   void llegadaPersonaPiso();
+   void entradaPersonaAscensor();
+   void cambioPisoAscensor();
 
 #endif // EVENTO_H
