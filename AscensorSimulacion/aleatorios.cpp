39aa637b4ba6431d0d4fb51291b806aac64c428c
Edwin Gamboa <edwjov92@gmail.com>
2014-11-30 00:00:33 -0500

    Agregada primera version de generacion de enteros Aleatorios con distribucion uniforme y exponencial. Generadores disponibles: Mersenne y Mother.

--- AscensorSimulacion/aleatorios.cpp
+++ AscensorSimulacion/aleatorios.cpp
@@ -1,5 +1,25 @@
 #include "aleatorios.h"
 
-Aleatorios::Aleatorios()
-{
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include "RandomGenerator/mersenne.cpp"
+#include "RandomGenerator/mother.cpp"
+
+
+
+
+
+Aleatorios::Aleatorios(){    
+}
+
+int Aleatorios::generarEnteroAleatorioUniforme(){
+    // Declare object of combined generator
+    int seed = (int)time(0);
+    CRandomMersenne RanGen(seed);       // make instance of random number generator
+    return RanGen.IRandom(0,99);
+}
+
+int Aleatorios::generarEnteroAleatorioExponencial(double parametro){
+    return (-1/parametro)*log(parametro);
 }
--- AscensorSimulacion/aleatorios.h
+++ AscensorSimulacion/aleatorios.h
@@ -1,10 +1,14 @@
 #ifndef ALEATORIOS_H
 #define ALEATORIOS_H
 
+
 class Aleatorios
 {
 public:
     Aleatorios();
+    int generarEnteroAleatorioUniforme();
+    int generarEnteroAleatorioExponencial(double parametro);
+    
 };
 
 #endif // ALEATORIOS_H
